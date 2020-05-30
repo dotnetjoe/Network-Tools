5c6dd7375b3950132da857fc45eef5c9f22b0492
ronaldogdm <ronaldogarciadem@gmail.com>
2020-05-30 00:43:23 -0400

    ping refactored

--- tools/ping.py
+++ tools/ping.py
@@ -1,6 +1,5 @@
-import time
-
 from networks.utils import send_icmp_packet
+import time
 
 
 def ping(host, count=5, ttl=64, timeout=5, verbose=True):
@@ -14,7 +13,7 @@ def ping(host, count=5, ttl=64, timeout=5, verbose=True):
         :return: whether the host responded to one of the packets that sent to him.
         :rtype: bool
     """
-    
+
     host_up = False
 
     for i in range(count):
@@ -23,10 +22,13 @@ def ping(host, count=5, ttl=64, timeout=5, verbose=True):
         end_time = time.time()
         response_time = end_time - start_time
         host_up = bool(response_packet)
+
         if host_up:
             message = f"Reply from {host}: time={response_time}s TTL={response_packet.ttl}"
         else:
             message = "Request timed out."
+
         if verbose:
             print(message)
+
     return host_up
